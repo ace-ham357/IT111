eb3e80e6fe616687e0964f4ee5375f43117a2544
ace-ham357 <achamilton2545@gmail.com>
2025-05-16 16:44:28 -0700

    Add files via upload

---
+++ dailySpecialsV2.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class dailySpecialsV2 {
+    public static void main(String[] args) {
+
+        String specials;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a week day (Monday - Friday)");
+
+        specials = input.next();
+
+        String coffee = null;
+
+        double price = 0;
+
+        boolean wrongDay = true;
+
+
+        while (wrongDay){
+            System.out.println("Please enter a weekday (not a weekend) or check your spelling");
+
+        specials = input.next();
+
+        switch (specials) {
+            case "Monday" -> {
+                wrongDay = false;
+                coffee = "Latte";
+                price = 4.95;
+                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+            }
+            case "Tuesday" -> {
+                wrongDay = false;
+                coffee = "Cappuccino";
+                price = 4.95;
+                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+            }
+            case "Wednesday" -> {
+                wrongDay = false;
+                coffee = "Flat White";
+                price = 5.45;
+                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+            }
+            case "Thursday" -> {
+                wrongDay = false;
+                coffee = "Mocha";
+                price = 5.45;
+                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+            }
+            case "Friday" -> {
+                wrongDay = false;
+                coffee = "Salted Caramel Latte";
+                price = 5.95;
+                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+            }
+        }
+    }
+
+    System.out.println("How many " + coffee + "s would you like to order?");
+
+           int order = input.nextInt();
+           double totalPrice = price * order;
+
+            if (order == 0) {
+                System.out.println("Looks like you don't like " + coffee + "s. So sad!!!");
+            } else if (order == 1) {
+                System.out.println("Looks like you will be ordering only 1 " + coffee + " today!");
+            } else {
+                System.out.println(order + " " + coffee + "s have been ordered, totalling");
+                System.out.printf("$%.2f", totalPrice);
+                System.out.println(" dollars.");
+            }
+
+        }
+    }
+
+
