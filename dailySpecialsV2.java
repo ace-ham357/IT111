712e6aa14a9d6ed1f2bc9bbda5b9fd0c85a7f5ee
ace-ham357 <achamilton2545@gmail.com>
2025-05-16 17:04:10 -0700

    Update dailySpecialsV2.java
    Added comments

--- dailySpecialsV2.java
+++ dailySpecialsV2.java
@@ -56,7 +56,7 @@ public class dailySpecialsV2 {
         }
     }
 
-    System.out.println("How many " + coffee + "s would you like to order?");
+        System.out.println("How many " + coffee + "s would you like to order?");
 
            int order = input.nextInt();
            double totalPrice = price * order;
@@ -71,7 +71,50 @@ public class dailySpecialsV2 {
                 System.out.println(" dollars.");
             }
 
-        }
-    }
+     }
+ }
+// I feel like should admit that my first attempt did not use the nested switch or a while loop. 
+// Each of the switch cases contained the "How many?" question and the final chunk of if-else statements. 
+// Like so:
+//        String specials;
+//        Scanner input = new Scanner(System.in);
+
+//        System.out.println("Please enter a week day (Monday - Friday)");
+//
+//        specials = input.next();
+//
+//        String coffee;
+//        double price;
+//        double totalPrice;
+//        int order;
+//        
+//        boolean saturday =specials.equals("Saturday");
+//        boolean sunday =specials.equals("Sunday");
+//
+//        if(saturday || sunday){
+//            System.out.println("Please enter a weekday, not a weekend.");
+//            specials = input.next();
+//        }
+//        switch (specials) {
+//            case "Monday" -> {
+//                coffee = "Latte";
+//                price = 4.95;
+//                System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
+//                System.out.println("How many " + coffee + "s would you like to order?");
+//                order = input.nextInt();
+//                totalPrice = price * order;
+//
+//                if (order == 0) {
+//                    System.out.println("Looks like you don't like " + coffee + "s. So sad!!!");
+//                } else if (order == 1) {
+//                    System.out.println("Looks like you will be ordering only 1 " + coffee + " today!");
+//                } else {
+//                    System.out.println(order + " " + coffee + "s have been ordered, totalling");
+//                    System.out.printf("$%.2f", totalPrice);
+//                    System.out.println(" dollars.");
+//                }
+//            }
+//         case "Tuesday" -> {
+//         ... etc.
 
 
